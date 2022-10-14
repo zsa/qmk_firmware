9cf5b50e2032dc2be86fd4b4dea6b6439eab599e
Drashna Jael're <drashna@live.com>
2022-10-14 15:30:00 -0700

    Reduce i2c timeouts for rgb led supported hardware

--- keyboards/moonlander/post_config.h
+++ keyboards/moonlander/post_config.h
@@ -1,9 +1,20 @@
+#pragma once
+
 #ifdef AUDIO_ENABLE
-#ifndef STARTUP_SONG
-#    define STARTUP_SONG SONG(E__NOTE(_DS5), E__NOTE(_D5), E__NOTE(_AS4), Q__NOTE(_F5))
+#    ifndef STARTUP_SONG
+#        define STARTUP_SONG SONG(E__NOTE(_DS5), E__NOTE(_D5), E__NOTE(_AS4), Q__NOTE(_F5))
+#    endif
+
+#    ifndef GOODBYE_SONG
+#        define GOODBYE_SONG SONG(E__NOTE(_D5), E__NOTE(_F5), E__NOTE(_C5), Q__NOTE(_AS4))
+#    endif
 #endif
 
-#ifndef GOODBYE_SONG
-#    define GOODBYE_SONG SONG(E__NOTE(_D5), E__NOTE(_F5), E__NOTE(_C5), Q__NOTE(_AS4))
-#endif
+#ifdef RGB_MATIRX_ENABLE
+#    ifndef ISSI_TIMEOUT
+#        define ISSI_TIMEOUT 5
+#    endif
+#    if !defined(MOONLANDER_I2C_TIMEOUT) && ISSI_TIMEOUT < 10
+#        define MOONLANDER_I2C_TIMEOUT (10 - ISSI_TIMEOUT)
+#    endif
 #endif
