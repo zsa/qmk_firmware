13ee679c99cda35f25be08be5d451ade287138e8
Florian Didron <0x6664@hey.com>
2023-01-23 13:28:39 +0900

    feat: adds halfmoon

--- keyboards/moonlander/matrix.c
+++ keyboards/moonlander/matrix.c
@@ -87,7 +87,11 @@ void matrix_init_custom(void) {
     setPinInputLow(A7);
     setPinInputLow(B0);
 
+    #ifdef HALFMOON
+    is_launching = true;
+    #else
     mcp23018_init();
+    #endif
 }
 
 bool matrix_scan_custom(matrix_row_t current_matrix[]) {
@@ -216,9 +220,12 @@ void matrix_power_up(void) {
         ML_LED_1(false);
         ML_LED_2(false);
         ML_LED_3(false);
+
+        #ifndef HALFMOON
         ML_LED_4(false);
         ML_LED_5(false);
         ML_LED_6(false);
+        #endif
     }
 
     // initialize matrix state: all keys off
