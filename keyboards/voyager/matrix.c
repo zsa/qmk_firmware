ca7bfe64c1450e60203fbcc9805c39cb8a02418a
Florian Didron <fd@0x6664.com>
2023-05-16 09:32:31 +0800

    chore: comments and code cleanup

--- keyboards/voyager/matrix.c
+++ keyboards/voyager/matrix.c
@@ -14,18 +14,13 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ */
 
 #include "voyager.h"
 #include "i2c_master.h"
 
-/*
-#define MATRIX_ROW_PINS { B10, B11, B12, B13, B14, B15 } outputs
-#define MATRIX_COL_PINS { A0, A1, A2, A3, A6, A7, B0 }   inputs
- */
-/* matrix state(1:on, 0:off) */
-extern matrix_row_t matrix[MATRIX_ROWS];      // debounced values
-extern matrix_row_t raw_matrix[MATRIX_ROWS];  // raw values
+extern matrix_row_t matrix[MATRIX_ROWS];     // debounced values
+extern matrix_row_t raw_matrix[MATRIX_ROWS]; // raw values
 static matrix_row_t raw_matrix_right[MATRIX_COLS];
 
 #define ROWS_PER_HAND (MATRIX_ROWS / 2)
@@ -45,19 +40,16 @@ uint8_t mcp23018_rx[1];
 void mcp23018_init(void) {
     i2c_init();
 
-    // #define MCP23_ROW_PINS { GPB5, GBP4, GBP3, GBP2, GBP1, GBP0 }       outputs
-    // #define MCP23_COL_PINS { GPA0, GBA1, GBA2, GBA3, GBA4, GBA5, GBA6 } inputs
-
-    mcp23018_tx[0] = 0x00;        // IODIRA
-    mcp23018_tx[1] = 0b00000000;  // A is output
-    mcp23018_tx[2] = 0b00111111;  // B is inputs
+    mcp23018_tx[0] = 0x00;       // IODIRA
+    mcp23018_tx[1] = 0b00000000; // A is output
+    mcp23018_tx[2] = 0b00111111; // B is inputs
 
     if (MSG_OK != i2c_transmit(MCP23018_DEFAULT_ADDRESS << 1, mcp23018_tx, 3, VOYAGER_I2C_TIMEOUT)) {
         dprintf("error hori\n");
     } else {
-        mcp23018_tx[0] = 0x0C;        // GPPUA
-        mcp23018_tx[1] = 0b10000000;  // A is not pulled-up
-        mcp23018_tx[2] = 0b11111111;  // B is pulled-up
+        mcp23018_tx[0] = 0x0C;       // GPPUA
+        mcp23018_tx[1] = 0b10000000; // A is not pulled-up
+        mcp23018_tx[2] = 0b11111111; // B is pulled-up
         wait_ms(5);
 
         if (MSG_OK != i2c_transmit(MCP23018_DEFAULT_ADDRESS << 1, mcp23018_tx, 3, VOYAGER_I2C_TIMEOUT)) {
@@ -95,19 +87,16 @@ void matrix_init_custom(void) {
 bool matrix_scan_custom(matrix_row_t current_matrix[]) {
     bool changed = false;
 
-    // Try to re-init right side
+    // Attempt to reset the mcp23018 if it's not initialized
     if (!mcp23018_initd) {
         if (++mcp23018_reset_loop == 0) {
-            // if (++mcp23018_reset_loop >= 1300) {
-            // since mcp23018_reset_loop is 8 bit - we'll try to reset once in 255 matrix scans
-            // this will be approx bit more frequent than once per second
-            print("trying to reset mcp23018\n");
+            // Since mcp23018_reset_loop is 8 bit - we'll try to reset once in 255 matrix scans. This will be approx bit more frequent than once per second.
             mcp23018_init();
-            if (!mcp23018_initd) {
-                print("right side not responding\n");
-            } else {
+            if (mcp23018_initd) {
+                // If we managed to initialize the mcp23018 - we need to reinitialize the matrix / layer state.
                 matrix_init();
                 layer_state_set(0);
+                // We also need to reinitialize the RGB matrix if it's enabled.
 #ifdef RGB_MATRIX_ENABLE
                 rgb_matrix_init();
 #endif
@@ -115,84 +104,101 @@ bool matrix_scan_custom(matrix_row_t current_matrix[]) {
         }
     }
 
-        matrix_row_t data = 0;
-        // actual matrix
-        for (uint8_t row = 0; row <= ROWS_PER_HAND; row++) {
-            // strobe row
+    // Scanning left and right side of the keyboard for key presses.
+    // Left side is scanned by reading the gpio pins directly, right side is scanned by reading the mcp23018 registers.
+    // In order to give enought time between selecting rows over i2c and reading the columns, we're scanning the left side first between the two i2c transactions.
+    matrix_row_t data = 0;
+    for (uint8_t row = 0; row <= ROWS_PER_HAND; row++) {
+        // strobe row
+        switch (row) {
+            case 0:
+                writePinHigh(B10);
+                break;
+            case 1:
+                writePinHigh(B11);
+                break;
+            case 2:
+                writePinHigh(B12);
+                break;
+            case 3:
+                writePinHigh(B13);
+                break;
+            case 4:
+                writePinHigh(B14);
+                break;
+            case 5:
+                writePinHigh(B15);
+                break;
+            case 6:
+                break; // Left hand has 6 rows
+        }
+
+        // Selecting the row on the right side of the keyboard.
+        if (mcp23018_initd) {
+            mcp23018_tx[0] = 0x12;                                                                  // GPIOA
+            mcp23018_tx[1] = (0b01111111 & ~(1 << (row))) | ((uint8_t)!mcp23018_leds[2] << 7);      // activate row
+            mcp23018_tx[2] = ((uint8_t)!mcp23018_leds[1] << 6) | ((uint8_t)!mcp23018_leds[0] << 7); // activate row
+
+            if (MSG_OK != i2c_transmit(MCP23018_DEFAULT_ADDRESS << 1, mcp23018_tx, 3, VOYAGER_I2C_TIMEOUT)) {
+                mcp23018_initd = false;
+            }
+        }
+
+        // Reading the left side of the keyboard.
+        if (row < ROWS_PER_HAND) {
+            // i2c comm incur enough wait time
+            if (!mcp23018_initd) {
+                // need wait to settle pin state
+                matrix_io_delay();
+            }
+            // read col data
+            data = ((readPin(A0) << 0) | (readPin(A1) << 1) | (readPin(A2) << 2) | (readPin(A3) << 3) | (readPin(A6) << 4) | (readPin(A7) << 5) | (readPin(B0) << 6));
+            // unstrobe  row
             switch (row) {
-                case 0: writePinHigh(B10); break;
-                case 1: writePinHigh(B11); break;
-                case 2: writePinHigh(B12); break;
-                case 3: writePinHigh(B13); break;
-                case 4: writePinHigh(B14); break;
-                case 5: writePinHigh(B15); break;
-                case 6: break; // Left hand has 6 rows
+                case 0:
+                    writePinLow(B10);
+                    break;
+                case 1:
+                    writePinLow(B11);
+                    break;
+                case 2:
+                    writePinLow(B12);
+                    break;
+                case 3:
+                    writePinLow(B13);
+                    break;
+                case 4:
+                    writePinLow(B14);
+                    break;
+                case 5:
+                    writePinLow(B15);
+                    break;
+                case 6:
+                    break;
             }
 
-            // right side
-            if (mcp23018_initd) {
-
-                // select row
-                mcp23018_tx[0] = 0x12;                                                                   // GPIOA
-                mcp23018_tx[1] = (0b01111111 & ~(1 << (row))) | ((uint8_t)!mcp23018_leds[2] << 7);       // activate row
-                mcp23018_tx[2] = ((uint8_t)!mcp23018_leds[1] << 6) | ((uint8_t)!mcp23018_leds[0] << 7);  // activate row
-
-                if (MSG_OK != i2c_transmit(MCP23018_DEFAULT_ADDRESS << 1, mcp23018_tx, 3, VOYAGER_I2C_TIMEOUT)) {
-                    dprintf("error hori\n");
-                    mcp23018_initd = false;
-                }
+            if (current_matrix[row] != data) {
+                current_matrix[row] = data;
+                changed             = true;
             }
-            // left side
-            if (row < ROWS_PER_HAND) {
-                // i2c comm incur enough wait time
-                if (!mcp23018_initd) {
-                    // need wait to settle pin state
-                    matrix_io_delay();
-                }
-                // read col data
-                data = (
-                    (readPin(A0) << 0 ) |
-                    (readPin(A1) << 1 ) |
-                    (readPin(A2) << 2 ) |
-                    (readPin(A3) << 3 ) |
-                    (readPin(A6) << 4 ) |
-                    (readPin(A7) << 5 ) |
-                    (readPin(B0) << 6 )
-                );
-                // unstrobe  row
-                switch (row) {
-                    case 0: writePinLow(B10); break;
-                    case 1: writePinLow(B11); break;
-                    case 2: writePinLow(B12); break;
-                    case 3: writePinLow(B13); break;
-                    case 4: writePinLow(B14); break;
-                    case 5: writePinLow(B15); break;
-                    case 6: break;
-                }
+        }
 
-                if (current_matrix[row] != data) {
-                    current_matrix[row]    = data;
-                    changed                = true;
-                }
-            }
-            if (mcp23018_initd) {
-
-                // read col
-                mcp23018_tx[0] = 0x13;  // GPIOB
-                if (MSG_OK != i2c_readReg(MCP23018_DEFAULT_ADDRESS << 1, mcp23018_tx[0], &mcp23018_rx[0], 1, VOYAGER_I2C_TIMEOUT)) {
-                    dprintf("error vert\n");
-                    mcp23018_initd = false;
-                }
-
-                data = ~(mcp23018_rx[0] & 0b00111111);
-            } else {
-                data = 0;
+        // Reading the right side of the keyboard.
+        if (mcp23018_initd) {
+            mcp23018_tx[0] = 0x13; // GPIOB
+            if (MSG_OK != i2c_readReg(MCP23018_DEFAULT_ADDRESS << 1, mcp23018_tx[0], &mcp23018_rx[0], 1, VOYAGER_I2C_TIMEOUT)) {
+                mcp23018_initd = false;
             }
 
-            if (raw_matrix_right[row] != data) {
-                raw_matrix_right[row] = data;
-                changed         = true;
-            }
+            data = ~(mcp23018_rx[0] & 0b00111111);
+        } else {
+            data = 0;
+        }
+
+        if (raw_matrix_right[row] != data) {
+            raw_matrix_right[row] = data;
+            changed               = true;
+        }
     }
 
     for (uint8_t row = 0; row < ROWS_PER_HAND; row++) {
@@ -220,10 +226,9 @@ void matrix_power_up(void) {
     }
 
     // initialize matrix state: all keys off
-    for (uint8_t i=0; i < MATRIX_ROWS; i++) {
+    for (uint8_t i = 0; i < MATRIX_ROWS; i++) {
         matrix[i] = 0;
     }
-
 }
 
 bool is_transport_connected(void) {
