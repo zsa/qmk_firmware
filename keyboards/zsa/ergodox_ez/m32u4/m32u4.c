f616fb68ea533a3cb020b7ce2edf3e4c31e99bfb
Drashna Jael're <drashna@live.com>
2024-11-25 10:35:28 -0800

    Revert "fix: shine led indicator brightness"
    This reverts commit 0b6f7f996cbd9f2eddde5413baf937cd70efb5d1.

--- keyboards/zsa/ergodox_ez/m32u4/m32u4.c
+++ keyboards/zsa/ergodox_ez/m32u4/m32u4.c
@@ -1,64 +1,17 @@
 // Copyright 2023 Christopher Courtney, aka Drashna Jael're  (@drashna) <drashna@live.com>
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-#include <stdint.h>
 #include "ergodox_ez.h"
 
-static uint8_t ergodox_right_led_1_duty;
-static uint8_t ergodox_right_led_2_duty;
-static uint8_t ergodox_right_led_3_duty;
-
 void ergodox_right_led_1_set(uint8_t n) {
-    ergodox_right_led_1_duty = n;
-    if (ergodox_right_led_1_duty == 0) {
-        ergodox_right_led_1_off();
-    } else {
-        ergodox_right_led_1_on();
-    }
+    OCR1A = n;
 }
-
-void ergodox_right_led_1_on(void) {
-    OCR1A = ergodox_right_led_1_duty;
-}
-
-void ergodox_right_led_1_off(void) {
-    OCR1A = 0;
-}
-
 void ergodox_right_led_2_set(uint8_t n) {
-    ergodox_right_led_2_duty = n;
-    if (ergodox_right_led_2_duty == 0) {
-        ergodox_right_led_2_off();
-    } else {
-        ergodox_right_led_2_on();
-    }
+    OCR1B = n;
 }
-
-void ergodox_right_led_2_on(void) {
-    OCR1B = ergodox_right_led_2_duty;
-}
-
-void ergodox_right_led_2_off(void) {
-    OCR1B = 0;
-}
-
 void ergodox_right_led_3_set(uint8_t n) {
-    ergodox_right_led_3_duty = n;
-    if (ergodox_right_led_3_duty == 0) {
-        ergodox_right_led_3_off();
-    } else {
-        ergodox_right_led_3_on();
-    }
+    OCR1C = n;
 }
-
-void ergodox_right_led_3_off(void) {
-    OCR1C = 0;
-}
-
-void ergodox_right_led_3_on(void) {
-    OCR1C = ergodox_right_led_3_duty;
-}
-
 
 void keyboard_post_init_sub(void) {
     // keyboard LEDs (see "PWM on ports OC1(A|B|C)" in "teensy-2-0.md")
