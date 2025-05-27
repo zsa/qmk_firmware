f2142060077a3f4f59d1997e24cbdd32ead2a3f3
Drashna Jael're <drashna@live.com>
2025-05-26 23:45:09 -0700

    Soft reset matrix

--- keyboards/zsa/moonlander/matrix.c
+++ keyboards/zsa/moonlander/matrix.c
@@ -46,6 +46,15 @@ bool io_expander_ready(void) {
     return mcp23018_read_pins(MCP23018_DEFAULT_ADDRESS, mcp23018_PORTA, &tx);
 }
 
+uint8_t mcp23018_init_local(void) {
+    uint8_t errors = 0;
+    mcp23018_init(MCP23018_DEFAULT_ADDRESS);
+    errors += !mcp23018_set_config(MCP23018_DEFAULT_ADDRESS, mcp23018_PORTA, 0b00000000);
+    errors += !mcp23018_set_config(MCP23018_DEFAULT_ADDRESS, mcp23018_PORTB, 0b00111111);
+
+    return errors;
+}
+
 void matrix_init_custom(void) {
     // outputs
     gpio_set_pin_output(B10);
@@ -64,9 +73,7 @@ void matrix_init_custom(void) {
     gpio_set_pin_input_low(A7);
     gpio_set_pin_input_low(B0);
 
-    mcp23018_init(MCP23018_DEFAULT_ADDRESS);
-    mcp23018_errors += !mcp23018_set_config(MCP23018_DEFAULT_ADDRESS, mcp23018_PORTA, 0b00000000);
-    mcp23018_errors += !mcp23018_set_config(MCP23018_DEFAULT_ADDRESS, mcp23018_PORTB, 0b00111111);
+    mcp23018_errors = mcp23018_init_local();
 
     if (!mcp23018_errors) {
         is_launching = true;
@@ -80,8 +87,11 @@ bool matrix_scan_custom(matrix_row_t current_matrix[]) {
         if (++mcp23018_reset_loop > 0x1FFF) {
             if (io_expander_ready()) {
                 // If we managed to initialize the mcp23018 - we need to reinitialize the matrix / layer state. During an electric discharge the i2c peripherals might be in a weird state. Giving a delay and resetting the MCU allows to recover from this.
-                wait_ms(200);
-                mcu_reset();
+                mcp23018_reset_loop = 0;
+                mcp23018_errors     = mcp23018_init_local();
+#ifdef RGB_MATRIX_ENABLE
+                rgb_matrix_init();
+#endif
             }
         }
     }
